ac938f6f02eb358db9f3f53336f8bd9f0e2ffaca
colacedavide <colace.davide00@gmail.com>
2025-12-09 17:31:00 +0100

    funzione saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function NomeUser(name){
+    return "Ciao "+ name;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(NomeUser(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
