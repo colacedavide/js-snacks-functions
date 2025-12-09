0a66c1b517eccbb832d58b617db550e744ce8c37
colacedavide <colace.davide00@gmail.com>
2025-12-09 17:49:00 +0100

    2nd esercizio

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function Iniziali(array){
+    let parolaIniziale = [];
 
+    for(let i = 0; i < array.lenght; i++){
+        
+        const primaParola = array[i].charAt(0);
+
+        parolaIniziale.push(primaParola);
+    }
+    
+    return parolaIniziale;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let primaLettera = Iniziali(names);
+console.log(primaLettera);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
